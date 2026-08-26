78354df5211c31a0e7cb3246ae6d1f06ed8445ed
ELUMBA-maker <elumba237@gmail.com>
2026-08-26 22:31:47 +0100

    added the authController middleware

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 
-const { Pool } = require('pg');
-require('dotenv').config();
+import pkg from 'pg';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const { Pool } = pkg;
 
 const pool = process.env.DATABASE_URL
   ? new Pool({
@@ -19,15 +22,16 @@ const pool = process.env.DATABASE_URL
       port: process.env.DB_PORT || 5432,
     });
 
-// Verify connection on startup
 pool
   .connect()
   .then((client) => {
-    console.log(' PostgreSQL connected successfully');
+    console.log('PostgreSQL connected successfully');
     client.release();
   })
   .catch((err) => {
-    console.error(' PostgreSQL connection error:', err.message);
+    console.error('PostgreSQL connection error:', err.message);
   });
 
-module.exports = pool;
+export const query = (text, params) => pool.query(text, params);
+
+export default pool;
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import express from 'express'
 import authRoutes from './routes/authRoutes.js'
 import categoryRoutes from './routes/categoryRoutes.js';
-app.use('/categories', categoryRoutes);
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use('/auth', authRoutes)
+app.use('/categories', categoryRoutes)
 
 app.get('/health', (req, res) =>{
     res.send({message:"Server health good"});
 
 } )
 
-app.use('/auth', authRoutes)
 
 app.listen(port, ()=>{
     console.log(`server running on: http://localhost:${port}`)
