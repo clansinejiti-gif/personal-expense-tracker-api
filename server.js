0e68079b4387730547565ddc6dce342d76aad301
SyntaxSultan <clansinejiti@gmail.com>
2026-08-26 19:23:00 +0100

    Created the the auth route and imported in the main sever.js file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import authRoutes from './routes/authRoutes.js'
 const app = express();
 const port = 3000;
 
@@ -6,6 +7,8 @@ app.get('/health', (req, res) =>{
     res.send({message:"Server health good"});
 
 } )
+
+app.use('/auth', authRoutes)
 
 app.listen(port, ()=>{
     console.log(`server running on: http://localhost:${port}`)
